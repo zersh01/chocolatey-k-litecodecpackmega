14e63c726ab16f6e96d9d06d02344f817a15b95c
Sergei Parhomenko <zersh@mail.ru>
2017-09-03 21:09:17 +0300

    updChocoPack.pl for auto update 2

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,13 +1,17 @@
 ﻿$packageName = 'k-litecodecpackmega'
 $installerType = 'exe'
-$url = 'http://files3.codecguide.com/K-Lite_Codec_Pack_1295_Mega.exe'
+$url = 'http://files2.codecguide.com/K-Lite_Codec_Pack_1350_Mega.exe'
 $silentArgs = '/VERYSILENT'
 
-$checksum = 'e119174a76a96da06ddb31364b5dea60'
+$checksum = '6e9799e437af7e61fbf94db3d4d73227'
 
 $checksumType = 'md5'
  
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url"  -Checksum "$checksum" -ChecksumType "$checksumType"
+
+
+
+
 
 
 
--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -30,3 +30,7 @@ if ($uninstallString -ne "") {
 
 
 
+
+
+
+
