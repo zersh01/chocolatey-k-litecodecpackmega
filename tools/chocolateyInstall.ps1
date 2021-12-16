e37b118e6973f07929ee891f196aa410a667cf53
zersh01 <zersh01@gmail.com>
2021-12-16 21:45:13 +0300

    version 16.6.5

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = 'k-litecodecpackmega'
 $installerType = 'exe'
-$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1660_Mega.exe'
+$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1665_Mega.exe'
 
 $silentArgs = '/VERYSILENT'
 
-$checksum = '99f4c05edbc83ae2118554705a100133'
+$checksum = 'c6bd52c989117f513e50b1c760551565'
 
 $checksumType = 'md5'
  
