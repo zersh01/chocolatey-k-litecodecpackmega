9d1704e87f0c3b8192757705b3113a01b3e513f4
zersh01 <zersh01@gmail.com>
2023-11-12 22:01:49 +0300

    version 17.9.4

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = 'k-litecodecpackmega'
 $installerType = 'exe'
-$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1790_Mega.exe'
+$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1794_Mega.exe'
 
 $silentArgs = '/VERYSILENT'
 
-$checksum = 'a2210949a394ce555b1e9962547e528a'
+$checksum = '46e6fac847dd0b180e142d9a973ce775'
 
 $checksumType = 'md5'
  
