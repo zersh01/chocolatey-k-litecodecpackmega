9de12a85c2089e0eb42ff40d74c18af42d5acaae
zersh <zersh@mail.ru>
2026-01-10 15:37:15 +0300

    new ver 19.4.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$packageName = 'k-litecodecpackmega'
 $installerType = 'exe'
-$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1935_Mega.exe'
+$url = 'https://files3.codecguide.com/K-Lite_Codec_Pack_1940_Mega.exe'
 
 $silentArgs = '/VERYSILENT'
 
-$checksum = 'cbaaa0e7f0b4e8270dd671c9a9b11245'
+$checksum = 'b9a5744eb7ed8017f7b3af25fbf902bb'
 
 $checksumType = 'md5'
  
